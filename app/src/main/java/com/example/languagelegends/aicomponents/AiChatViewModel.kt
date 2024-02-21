d76ad6d6ea5f127b8d706eb48723477b97aa7ae4
Jere <101744771+JerbsH@users.noreply.github.com>
2024-02-21 14:29:48 +0200

    ADD: added some TODOs and comments

--- app/src/main/java/com/example/languagelegends/aicomponents/AiChatViewModel.kt
+++ app/src/main/java/com/example/languagelegends/aicomponents/AiChatViewModel.kt
@@ -10,18 +10,22 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 
+// Here are all the functionalities needed for Hexascibe VertexAI-KT to work (https://github.com/hexascribe/vertexai-kt)
 class AiChatViewModel : ViewModel() {
     var menuVisibility = MutableLiveData<Boolean>()
     var topic = MutableLiveData<String>()
     var response = MutableLiveData<String>()
 
+    //Initialize the VertexAI
+    //TODO: Replace the accessToken and projectID with values from a file or a secure location
     private val vertexAI by lazy {
         VertexAI.Builder()
             .setAccessToken("")
-            .setProjectId("onyx-elevator-414111")
+            .setProjectId("")
             .build()
     }
-
+    //Initialize the textRequest for VertexAI
+    //TODO: May need to change model for chatting
     private val textRequest by lazy {
         vertexAI.textRequest()
             .setModel("text-unicorn")
@@ -31,6 +35,7 @@ class AiChatViewModel : ViewModel() {
             .setTopP(0.8)
     }
 
+    //Get a question from the AI based on the chosen topic
     private suspend fun executeRequest(text: String): String {
         val resultFlow = MutableStateFlow<String?>(null)
 
@@ -57,12 +62,11 @@ class AiChatViewModel : ViewModel() {
     }
 
     fun onAskMeAQuestion() {
+        //TODO: Implement the logic to ask a different question everytime and check the response if it is correct
         viewModelScope.launch {
             response.value =
                 executeRequest("Ask me to translate phrase with the theme of ${topic.value}")
             Log.d("DBG", "Response: ${response.value}")
         }
     }
-
-
 }
