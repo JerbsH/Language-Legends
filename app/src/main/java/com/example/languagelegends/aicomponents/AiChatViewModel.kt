0f3148202b3b717a47c819b3ce5a6c8f953844f7
MiikaJok <miikajok@metropolia.fi>
2024-02-29 18:50:34 +0200

    FIX: function simplifying, commenting, accesstoken refresh logic fixing

--- app/src/main/java/com/example/languagelegends/aicomponents/AiChatViewModel.kt
+++ app/src/main/java/com/example/languagelegends/aicomponents/AiChatViewModel.kt
@@ -13,78 +13,134 @@ import com.google.gson.JsonObject
 import com.hexascribe.vertexai.VertexAI
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlinx.coroutines.withContext
 import java.io.ByteArrayInputStream
-import kotlin.coroutines.resume
 
 
+/**
+ * ViewModel class for managing AI chat functionalities using VertexAI-KT.
+ * @property application Application instance to access application resources.
+ */
 // Here are all the functionalities needed for Hexascibe VertexAI-KT to work (https://github.com/hexascribe/vertexai-kt)
 class AiChatViewModel(private val application: Application) : ViewModel() {
+
+    // Constants
+    companion object {
+        const val ACCESS_TOKEN = "accessToken"
+        const val PROJECT_ID = "projectId"
+        const val TOKEN_EXPIRATION_TIME = "tokenExpirationTime"
+        const val SERVICE_ACCOUNT_KEY_PATH = "keyfile.json"
+
+    }
+
+    // LiveData objects for managing UI state
     var menuVisibility = MutableLiveData<Boolean>()
     var topic = MutableLiveData<String>()
     var response = MutableLiveData<String>()
 
+    // Variables for managing VertexAI instance
     private var projectId: String? = null
     private var accessToken: String? = null
     private var tokenExpirationTime: Long = 0
-
-    //Initialize the VertexAI
-    //TODO: Replace the accessToken and projectID with values from a file or a secure location
     private var vertexAI: VertexAI? = null
-
     private val sharedPreferences: SharedPreferences =
         application.getSharedPreferences("MyPrefs", Context.MODE_PRIVATE)
 
     init {
         viewModelScope.launch {
-            val currentAccessToken = sharedPreferences.getString("accessToken", null)
-            val currentProjectId = sharedPreferences.getString("projectId", null)
-            val tokenExpirationTime = sharedPreferences.getLong("tokenExpirationTime", 0)
-            Log.d("DBG", "MyPrefs: $currentProjectId")
+            initializeVertexAI()
+        }
+    }
 
-            var currentTime = System.currentTimeMillis()
+    /**
+     * Builds a VertexAI instance with the provided access token and project ID.
+     * @param accessToken Access token for VertexAI.
+     * @param projectId Project ID for VertexAI.
+     */
+    private fun buildVertexAIInstance(accessToken: String?, projectId: String?) {
+        vertexAI = VertexAI.Builder()
+            .setAccessToken(accessToken ?: throw IllegalStateException("Access token is null"))
+            .setProjectId(projectId ?: throw IllegalStateException("Project ID is null"))
+            .build()
+    }
+
+    /**
+     * Initializes VertexAI instance by checking for existing access token and project ID,
+     * or generating a new one if necessary.
+     */
+    private suspend fun initializeVertexAI() {
+        try {
+            val currentTime = System.currentTimeMillis()
+            val (currentAccessToken, currentProjectId, tokenExpirationTime) = getSavedTokenInfo()
+
             if (currentAccessToken != null && currentProjectId != null && currentTime < tokenExpirationTime) {
                 accessToken = currentAccessToken
                 projectId = currentProjectId
                 this@AiChatViewModel.tokenExpirationTime = tokenExpirationTime
-                vertexAI = VertexAI.Builder()
-                    .setAccessToken(currentAccessToken)
-                    .setProjectId(currentProjectId)
-                    .build()
             } else {
                 val (newAccessToken, newProjectId) = generateAccessToken()
-                accessToken = newAccessToken
-                projectId = newProjectId
-                currentTime = System.currentTimeMillis() // update currentTime
-                this@AiChatViewModel.tokenExpirationTime =
-                    currentTime + 60 * 60 * 1000 // +1 hour to the current time
-                sharedPreferences.edit().apply {
-                    putString("accessToken", newAccessToken)
-                    putString("projectId", newProjectId)
-                    putLong("tokenExpirationTime", this@AiChatViewModel.tokenExpirationTime)
-                    apply()
-                }
-                vertexAI = VertexAI.Builder()
-                    .setAccessToken(newAccessToken)
-                    .setProjectId(newProjectId)
-                    .build()
+                saveTokenInfo(newAccessToken, newProjectId, currentTime)
             }
 
-            // Calculate the remaining time for the token to expire
-            currentTime = System.currentTimeMillis()
-            val remainingTimeMillis = tokenExpirationTime - currentTime
-            val remainingTimeSeconds = remainingTimeMillis / 1000
-            Log.d("DBG", "Remaining time for token to expire: $remainingTimeSeconds seconds")
+            buildVertexAIInstance(accessToken, projectId)
+            remainingTokenTime()
+        } catch (e: Exception) {
+            Log.e("DBG", "Error initializing VertexAI: ${e.message}")
         }
     }
 
+    /**
+     * Retrieves saved token information from shared preferences.
+     * @return Triple containing access token, project ID, and token expiration time.
+     */
+    private fun getSavedTokenInfo(): Triple<String?, String?, Long> {
+        val currentAccessToken = sharedPreferences.getString(ACCESS_TOKEN, null)
+        val currentProjectId = sharedPreferences.getString(PROJECT_ID, null)
+        val tokenExpirationTime = sharedPreferences.getLong(TOKEN_EXPIRATION_TIME, 0)
+        Log.d("DBG", "MyPrefs: $currentProjectId")
+        return Triple(currentAccessToken, currentProjectId, tokenExpirationTime)
+    }
+
+    /**
+     * Saves new token information to shared preferences.
+     */
+    private fun saveTokenInfo(
+        newAccessToken: String,
+        newProjectId: String,
+        currentTime: Long
+    ) {
+        accessToken = newAccessToken
+        projectId = newProjectId
+        this@AiChatViewModel.tokenExpirationTime =
+            currentTime + 60 * 60 * 1000 // +1 hour to the current time
+        sharedPreferences.edit().apply {
+            putString(ACCESS_TOKEN, newAccessToken)
+            putString(PROJECT_ID, newProjectId)
+            putLong(TOKEN_EXPIRATION_TIME, this@AiChatViewModel.tokenExpirationTime)
+            apply()
+        }
+    }
+
+    /**
+     * Logs the remaining time for the access token to expire.
+     */
+    private fun remainingTokenTime() {
+        val currentTime = System.currentTimeMillis()
+        val remainingTimeMillis = tokenExpirationTime - currentTime
+        val remainingTimeSeconds = remainingTimeMillis / 1000
+        Log.d("DBG", "Remaining time for token to expire: $remainingTimeSeconds seconds")
+    }
+
+    /**
+     * Generates a new access token and project ID.
+     * @return Pair containing new access token and project ID.
+     */
     private suspend fun generateAccessToken(): Pair<String, String> = withContext(Dispatchers.IO) {
-        val serviceAccountKeyPath = "keyfile.json"
         val targetScopes = listOf("https://www.googleapis.com/auth/cloud-platform")
 
-        val inputStream = application.assets.open(serviceAccountKeyPath)
+        val inputStream = application.assets.open(SERVICE_ACCOUNT_KEY_PATH)
         val jsonContent = inputStream.bufferedReader()
             .use { it.readText() }
 
@@ -101,16 +157,36 @@ class AiChatViewModel(private val application: Application) : ViewModel() {
 
         Log.d("DBG", "Generated access token: $tokenValue")
 
-        // Update the VertexAI instance with the new access token
-        vertexAI = VertexAI.Builder()
-            .setAccessToken(tokenValue)
-            .setProjectId(projectId)
-            .build()
+        buildVertexAIInstance(tokenValue, projectId)
 
         return@withContext Pair(tokenValue, projectId)
     }
 
-    //Initialize the textRequest for VertexAI
+    /**
+     * Executes a request to VertexAI with the provided text.
+     * @param text Text to be processed by VertexAI.
+     * @return Result from VertexAI.
+     */
+    private suspend fun executeRequest(text: String): String {
+        val resultFlow = MutableStateFlow<String?>(null)
+
+        viewModelScope.launch {
+            val currentTime = System.currentTimeMillis()
+            if (currentTime >= tokenExpirationTime) {
+                initializeVertexAI()
+            }
+
+            val result = textRequest.execute(text).getOrThrow()
+            resultFlow.value = result
+            Log.d("DBG", "Result: $result")
+        }
+
+        return resultFlow.first { it != null } ?: ""
+    }
+
+    /**
+     * Initializes the textRequest for VertexAI.
+     */
     //TODO: May need to change model for chatting
     private val textRequest by lazy {
         vertexAI?.textRequest()
@@ -121,38 +197,14 @@ class AiChatViewModel(private val application: Application) : ViewModel() {
             ?.setTopP(0.8) ?: throw IllegalStateException("VertexAI not initialized")
     }
 
-    //Get a question from the AI based on the chosen topic
-    private suspend fun executeRequest(text: String): String {
-        val resultFlow = MutableStateFlow<String?>(null)
-
-        viewModelScope.launch {
-            val result = textRequest.execute(text).getOrThrow()
-            resultFlow.value = result
-            Log.d("DBG", "Result: $result")
-        }
-
-        return suspendCancellableCoroutine { continuation ->
-            val collector = viewModelScope.launch {
-                resultFlow.collect { result ->
-                    if (result != null) {
-                        continuation.resume(result)
-                    }
-                }
-            }
-
-            continuation.invokeOnCancellation {
-                // Cancel the collector if coroutine is cancelled
-                collector.cancel()
-            }
-        }
-    }
-
+    /**
+     * Triggers a question to be asked by VertexAI based on the current topic.
+     */
     fun onAskMeAQuestion() {
         //TODO: Implement the logic to ask a different question everytime and check the response if it is correct
         viewModelScope.launch {
             response.value =
                 executeRequest("Ask me to translate phrase with the theme of ${topic.value}")
-            //Log.d("DBG", "Response: ${response.value}")
         }
     }
 }
