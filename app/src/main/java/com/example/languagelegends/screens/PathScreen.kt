812eb881b80ae33cae6efc02dae2fd8c22f7d3a2
Gresa Krasniqi <gresak@metropolia.fi>
2024-03-04 21:43:09 +0200

    Learning path view. Balls scattered and lost.

--- app/src/main/java/com/example/languagelegends/screens/PathScreen.kt
+++ app/src/main/java/com/example/languagelegends/screens/PathScreen.kt
@@ -3,15 +3,11 @@ package com.example.languagelegends.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -20,17 +16,44 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 import androidx.navigation.NavController
 
+
 @Composable
-fun PathScreen(navController : NavController) {
-    var completedExercises by remember { mutableStateOf(0) }
+fun PathScreen(navController: NavController) {
+    val completedExercises by remember { mutableStateOf(0) }
+    val scrollState = rememberScrollState()
 
     // Load the background image
     val backgroundImage = painterResource(id = com.example.languagelegends.R.drawable.path)
 
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(scrollState)
+    ) {
+        Image(
+            painter = backgroundImage,
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+        )
+
+        exercisePositions.forEachIndexed { index, (x, y) ->
+            LanguageExercise(
+                number = index + 1,
+                x = x,
+                y = y,
+                completedExercises = completedExercises,
+            ) {
+                // Navigate to the ExercisesScreen when exercise is clicked
+                navController.navigate("exercises/${it}")
+            }
+        }
+    }
+
+    /*
     Box(modifier = Modifier.fillMaxSize()) {
         // Background image
         Image(
@@ -52,7 +75,7 @@ fun PathScreen(navController : NavController) {
                     modifier = Modifier.fillMaxWidth()
                 ) {
                     // Add spacers on the sides
-                    Spacer(modifier = Modifier.weight(1f))
+                    //Spacer(modifier = Modifier.weight(1f))
                     // Render each number
                     LanguageExercise(
                         number = i,
@@ -67,16 +90,30 @@ fun PathScreen(navController : NavController) {
                 Spacer(modifier = Modifier.height(16.dp))
             }
         }
-    }
+    }*/
 }
 
+val exercisePositions = listOf(
+    Pair(100.dp, 100.dp), // 1
+    Pair(150.dp, 100.dp),
+    Pair(250.dp, 150.dp),
+    Pair(350.dp, 200.dp),
+    Pair(450.dp, 250.dp),
+    Pair(50.dp, 250.dp),
+    Pair(150.dp, 200.dp),
+    Pair(250.dp, 150.dp),
+    Pair(350.dp, 100.dp),
+    Pair(100.dp, 1000.dp) // 10
+)
 
 @Composable
 fun LanguageExercise(
+    modifier: Modifier = Modifier,
     number: Int,
+    x: Dp,
+    y: Dp,
     completedExercises: Int,
-    onClick: (Int) -> Unit
-
+    onClick: (Int) -> Unit,
 ) {
     // Determine if the exercise is unlocked
     val isUnlocked = number <= completedExercises + 1
@@ -87,14 +124,17 @@ fun LanguageExercise(
     // Render the circle as a clickable surface if the exercise is unlocked
     if (isUnlocked) {
         Surface(
-            modifier = Modifier.size(40.dp),
+            modifier = Modifier
+                .size(40.dp)
+                .offset(x, y),
             shape = CircleShape,
             color = circleColor,
             onClick = { onClick(number) } // Call the lambda onClick with the exercise number
         ) {
             Box(
                 contentAlignment = Alignment.Center,
-                modifier = Modifier.size(40.dp)
+                modifier = Modifier
+                    .size(40.dp)
             ) {
                 Text(
                     text = number.toString(),
@@ -106,7 +146,9 @@ fun LanguageExercise(
     } else {
         // Render the circle as a non-clickable surface if the exercise is locked
         Surface(
-            modifier = Modifier.size(40.dp),
+            modifier = Modifier
+                .size(40.dp)
+                .offset(x, y),
             shape = CircleShape,
             color = circleColor,
             onClick = {}
