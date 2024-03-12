19a52266a7675bbf807b38fa19e854cdc7468467
Gresa Krasniqi <gresak@metropolia.fi>
2024-03-12 21:20:04 +0200

    Points counter.

--- app/src/main/java/com/example/languagelegends/screens/PathScreen.kt
+++ app/src/main/java/com/example/languagelegends/screens/PathScreen.kt
@@ -1,9 +1,13 @@
 package com.example.languagelegends.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
@@ -12,15 +16,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.languagelegends.OnCompleteExercise
 import com.example.languagelegends.aicomponents.AiChatViewModel
+import com.example.languagelegends.database.DatabaseProvider
 
 @Composable
 
@@ -42,6 +51,8 @@ fun PathScreen(navController: NavController, apiSelectedLanguage: String, aiChat
             contentDescription = null,
             modifier = Modifier.fillMaxSize(),
         )
+        PointCounter(pointCount = totalCompletedExercises, modifier = Modifier.align(Alignment.TopStart))
+
         exercisePositions.forEachIndexed { index, (x, y) ->
             LanguageExercise(
                 number = index + 1,
@@ -133,5 +144,21 @@ fun LanguageExercise(
                 )
             }
         }
+    }
+}
+
+@Composable
+fun PointCounter(pointCount: Int, modifier: Modifier = Modifier) {
+
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .background(color = Color.Black.copy(alpha = 0.5f))
+    ) {
+        Text(
+            text = "Points: $pointCount",
+            color = Color.White,
+            modifier = Modifier.padding(8.dp)
+        )
     }
 }
