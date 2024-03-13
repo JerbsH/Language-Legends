29421a1de199bf52fdaf744ef4556c5322411c95
Jere <101744771+JerbsH@users.noreply.github.com>
2024-03-13 13:28:42 +0200

    FIX: broken merge and imports

--- app/src/main/java/com/example/languagelegends/screens/PathScreen.kt
+++ app/src/main/java/com/example/languagelegends/screens/PathScreen.kt
@@ -1,19 +1,10 @@
 package com.example.languagelegends.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -24,25 +15,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.derivedStateOf
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.languagelegends.R
 import com.example.languagelegends.OnCompleteExercise
 import com.example.languagelegends.aicomponents.AiChatViewModel
-import com.example.languagelegends.database.DatabaseProvider
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -65,7 +47,10 @@ fun PathScreen(navController: NavController, apiSelectedLanguage: String, aiChat
             contentDescription = null,
             modifier = Modifier.fillMaxSize(),
         )
-        PointCounter(pointCount = totalCompletedExercises, modifier = Modifier.align(Alignment.TopStart))
+        PointCounter(
+            pointCount = totalCompletedExercises,
+            modifier = Modifier.align(Alignment.TopStart)
+        )
 
         exercisePositions.forEachIndexed { index, (x, y) ->
             LanguageExercise(
@@ -79,99 +64,102 @@ fun PathScreen(navController: NavController, apiSelectedLanguage: String, aiChat
             }
         }
     }
-
-val exercisePositions = listOf(
-
-    Pair(0.22f, 2.84f),//ball 1
-    Pair(0.63f, 2.545f),
-    Pair(0.34f, 2.26f),
-    Pair(0.57f, 2.038f),
-    Pair(0.41f, 1.82f),
-    Pair(0.19f, 1.38f),
-    Pair(0.64f, 1.05f),
-    Pair(0.35f, 0.77f),
-    Pair(0.55f, 0.444f),
-    Pair(0.444f, 0.15f),// Ball 10
-)
-
-@Composable
-fun LanguageExercise(
-    number: Int,
-    x: Float,
-    y: Float,
-    completedExercises: Int,
-    onClick: (Int) -> Unit,
-) {
-    // Determine if the exercise is unlocked
-    val isUnlocked = number <= completedExercises + 1
-
-    // Determine the circle color based on the unlocked status
-    val circleColor = if (isUnlocked) Color(0xFF573C1A) else Color(0xFF996B2F)
-
-    // Calculate the positions as a percentage of the smallest width
-    val smallestWidth =
-        minOf(LocalConfiguration.current.screenWidthDp, LocalConfiguration.current.screenHeightDp)
-    val offsetX = x * smallestWidth
-    val offsetY = y * smallestWidth
-
-    // Render the circle as a clickable surface if the exercise is unlocked
-    if (isUnlocked) {
-        Surface(
-            modifier = Modifier
-                .size(40.dp)
-                .offset(offsetX.dp, offsetY.dp),
-            shape = CircleShape,
-            color = circleColor,
-            onClick = { onClick(number) } // Call the lambda onClick with the exercise number
-        ) {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier
-                    .fillMaxSize()
-            ) {
-                Text(
-                    text = number.toString(),
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = Color.White
-                )
-            }
-        }
-    } else {
-        // Render the circle as a non-clickable surface if the exercise is locked
-        Surface(
-            modifier = Modifier
-                .size(40.dp)
-                .offset(offsetX.dp, offsetY.dp),
-            shape = CircleShape,
-            color = circleColor,
-            onClick = {}
-        ) {
-            Box(
-                contentAlignment = Alignment.Center,
-                modifier = Modifier.fillMaxSize()
-            ) {
-                Text(
-                    text = number.toString(),
-                    style = MaterialTheme.typography.bodyLarge,
-                    color = Color.White
-                )
-            }
-        }
-    }
 }
+    val exercisePositions = listOf(
 
-@Composable
-fun PointCounter(pointCount: Int, modifier: Modifier = Modifier) {
+        Pair(0.22f, 2.84f),//ball 1
+        Pair(0.63f, 2.545f),
+        Pair(0.34f, 2.26f),
+        Pair(0.57f, 2.038f),
+        Pair(0.41f, 1.82f),
+        Pair(0.19f, 1.38f),
+        Pair(0.64f, 1.05f),
+        Pair(0.35f, 0.77f),
+        Pair(0.55f, 0.444f),
+        Pair(0.444f, 0.15f),// Ball 10
+    )
 
-    Column(
-        modifier = modifier
-            .padding(16.dp)
-            .background(color = Color.Black.copy(alpha = 0.5f))
+    @Composable
+    fun LanguageExercise(
+        number: Int,
+        x: Float,
+        y: Float,
+        completedExercises: Int,
+        onClick: (Int) -> Unit,
     ) {
-        Text(
-            text = "Points: $pointCount",
-            color = Color.White,
-            modifier = Modifier.padding(8.dp)
-        )
+        // Determine if the exercise is unlocked
+        val isUnlocked = number <= completedExercises + 1
+
+        // Determine the circle color based on the unlocked status
+        val circleColor = if (isUnlocked) Color(0xFF573C1A) else Color(0xFF996B2F)
+
+        // Calculate the positions as a percentage of the smallest width
+        val smallestWidth =
+            minOf(
+                LocalConfiguration.current.screenWidthDp,
+                LocalConfiguration.current.screenHeightDp
+            )
+        val offsetX = x * smallestWidth
+        val offsetY = y * smallestWidth
+
+        // Render the circle as a clickable surface if the exercise is unlocked
+        if (isUnlocked) {
+            Surface(
+                modifier = Modifier
+                    .size(40.dp)
+                    .offset(offsetX.dp, offsetY.dp),
+                shape = CircleShape,
+                color = circleColor,
+                onClick = { onClick(number) } // Call the lambda onClick with the exercise number
+            ) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    Text(
+                        text = number.toString(),
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = Color.White
+                    )
+                }
+            }
+        } else {
+            // Render the circle as a non-clickable surface if the exercise is locked
+            Surface(
+                modifier = Modifier
+                    .size(40.dp)
+                    .offset(offsetX.dp, offsetY.dp),
+                shape = CircleShape,
+                color = circleColor,
+                onClick = {}
+            ) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Text(
+                        text = number.toString(),
+                        style = MaterialTheme.typography.bodyLarge,
+                        color = Color.White
+                    )
+                }
+            }
+        }
     }
-}
+
+    @Composable
+    fun PointCounter(pointCount: Int, modifier: Modifier = Modifier) {
+
+        Column(
+            modifier = modifier
+                .padding(16.dp)
+                .background(color = Color.Black.copy(alpha = 0.5f))
+        ) {
+            Text(
+                text = "Points: $pointCount",
+                color = Color.White,
+                modifier = Modifier.padding(8.dp)
+            )
+        }
+    }
